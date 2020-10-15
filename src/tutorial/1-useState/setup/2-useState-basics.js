87cddb6c3a17553230ac853a9d493f5954d97bdd
Justyna Marciniak <justy.marciniak@gmail.com>
2020-10-15 20:21:41 +0900

    useState

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
-};
+  const [text, setText] = useState('random title')
+  const handleClick = () => {
+    text === 'random title' ? setText('another title') : setText('random title')
+  }
+  return (
+    <>
+      <h2>{text}</h2>
+      <button type='button' className='btn' onClick={handleClick}>
+        change title
+      </button>
+    </>
+  )
+}
 
-export default UseStateBasics;
+export default UseStateBasics
