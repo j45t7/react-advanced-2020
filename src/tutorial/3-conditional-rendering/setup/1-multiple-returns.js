29ffa0a1384298767ba7574c2218876202e1d4b9
Justyna Marciniak <justy.marciniak@gmail.com>
2020-10-16 21:44:12 +0900

    fetching data

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,52 @@
-import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
-const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
-};
+import React, { useState, useEffect } from 'react'
+// const url = 'https://api.github.com/users/QuincyLarson';
+const url = 'https://api.github.com/users/j45t7'
 
-export default MultipleReturns;
+const MultipleReturns = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [isError, setIsError] = useState(false)
+  const [user, setUser] = useState('default user')
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => {
+        if (res.status >= 200 && res.status <= 299) {
+          return res.json()
+        } else {
+          setIsLoading(false)
+          setIsError(true)
+          throw new Error(res.statusText)
+        }
+      })
+      .then((user) => {
+        const { id, login, avatar_url, html_url } = user
+        setUser(user)
+        setIsLoading(false)
+      })
+      .catch((error) => console.log(error))
+  }, [])
+
+  return isLoading ? (
+    <div>
+      <h2>Loading...</h2>
+    </div>
+  ) : isError ? (
+    <div>
+      <h2>Error...</h2>
+    </div>
+  ) : (
+    <div>
+      <ul className='users' key={user.id}>
+        <li>
+          <img src={user.avatar_url} alt={user.login} />
+          <div>
+            <h4>{user.login}</h4>
+            <a href={user.html_url}>profile</a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default MultipleReturns
