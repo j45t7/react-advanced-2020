7eca792493ea60d0a6594363c853726770876dd0
Justyna Marciniak <justy.marciniak@gmail.com>
2020-10-17 08:35:45 +0900

    ternary operator

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import UseEffectBasics from './tutorial/2-useEffect/setup/1-useEffect-basics'
 import UseEffectCleanup from './tutorial/2-useEffect/setup/2-useEffect-cleanup'
 import UseEffectFetchData from './tutorial/2-useEffect/setup/3-useEffect-fetch-data'
 import MultipleReturns from './tutorial/3-conditional-rendering/setup/1-multiple-returns'
+import ShortCircuit from './tutorial/3-conditional-rendering/setup/2-short-circuit'
 function App() {
   return (
     <div className='container'>
@@ -20,7 +21,8 @@ function App() {
       {/* <UseEffectBasics /> */}
       {/* <UseEffectCleanup /> */}
       {/* <UseEffectFetchData /> */}
-      <MultipleReturns />
+      {/* <MultipleReturns /> */}
+      <ShortCircuit />
     </div>
   )
 }
--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState('')
+  const [isError, setIsError] = useState(false)
+  // const firstValue = text || 'hello world'
+  // const secondValue = text && 'hello world'
+  return (
+    <>
+      {/* <h1>{firstValue}</h1> */}
+      {/* <h1>value: {secondValue}</h1> */}
+      <h1>{text || 'default'}</h1>
+      <button className='btn' onClick={() => setIsError(!isError)}>
+        toggle error
+      </button>
+      {isError && <h1>Error...</h1>}
+      {isError ? <p>there is an error..</p> : <p>no error</p>}
+    </>
+  )
+}
 
-  return <h2>short circuit</h2>;
-};
-
-export default ShortCircuit;
+export default ShortCircuit
