0a7b1c072079d7b30d90f88d12b4fd3021f22e48
Justyna Marciniak <justy.marciniak@gmail.com>
2020-10-17 08:46:28 +0900

    toggle component

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import UseEffectCleanup from './tutorial/2-useEffect/setup/2-useEffect-cleanup'
 import UseEffectFetchData from './tutorial/2-useEffect/setup/3-useEffect-fetch-data'
 import MultipleReturns from './tutorial/3-conditional-rendering/setup/1-multiple-returns'
 import ShortCircuit from './tutorial/3-conditional-rendering/setup/2-short-circuit'
+import ShowHide from './tutorial/3-conditional-rendering/setup/3-show-hide'
 function App() {
   return (
     <div className='container'>
@@ -22,7 +23,8 @@ function App() {
       {/* <UseEffectCleanup /> */}
       {/* <UseEffectFetchData /> */}
       {/* <MultipleReturns /> */}
-      <ShortCircuit />
+      {/* <ShortCircuit /> */}
+      <ShowHide />
     </div>
   )
 }
